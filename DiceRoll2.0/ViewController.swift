a0961ab3afffbc9f46631123f8b109d20edb387e
Lavanya Subramanian <lali.subramanian2005@gmail.com>
2019-07-18 09:40:33 -0700

    dice roll final game - no errors (i hope)

--- DiceRoll2.0/ViewController.swift
+++ DiceRoll2.0/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  DiceRoll2.0
+//  DiceRoll
 //
 //  Created by 23LavanyaSubramanian on 7/17/19.
 //  Copyright © 2019 Lavanya Subramanian. All rights reserved.
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
+    var dice = ["Dice Red 1", "Dice Red 2", "Dice Red 3", "Dice Red 4", "Dice Red 5", "Dice Red 6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func rollDice(_ sender: Any) {
+        
+        let rndNum = Int.random(in: 0...5)
+        let rndNum2 = Int.random(in: 0...5)
+        imageView1.image = UIImage(named :dice[rndNum])
+        imageView2.image = UIImage(named :dice[rndNum2])
+        
+    }
 }
-
